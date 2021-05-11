1bb53dfffd1a5b9d3db47f86fdb5e8ca2aef7aef
HP <s.k.vrema1655@gmail.com>
2021-05-11 09:46:47 +0530

    Main resource commited the code

--- src/test/java/interviewcode/Demo/DuplicateCharString.java
+++ src/test/java/interviewcode/Demo/DuplicateCharString.java
@@ -10,31 +10,30 @@ public class DuplicateCharString {
 		// TODO Auto-generated method stub
 
 		String str = "amma";
-		
+
 		char[] ch = str.toCharArray();
 		Map<Character, Integer> mapValue = new HashMap<Character, Integer>();
-		
+
 		for(Character itrChar:ch) {
-			
+
 			if(mapValue.containsKey(itrChar)) {
-				
+
 				mapValue.put(itrChar, mapValue.get(itrChar)+1);
 			}
 			else {
-				
+
 				mapValue.put(itrChar, 1);
 			}
 		}
-		
+
 		System.out.println(mapValue);
-		
-		/*
-		 * Set<Map.Entry<Character, Integer>> setvalue = mapValue.entrySet();
-		 * for(Map.Entry<Character, Integer> itrMap:setvalue) {
-		 * 
-		 * if(itrMap.getValue()>1) {
-		 * 
-		 * System.out.println(itrMap.getKey()+" "+itrMap.getValue()); } }
-		 */
+
+		/*Set<Map.Entry<Character, Integer>> setvalue = mapValue.entrySet();
+		for(Map.Entry<Character, Integer> itrMap:setvalue) {
+
+			if(itrMap.getValue()>1) {
+
+				System.out.println(itrMap.getKey()+" "+itrMap.getValue()); } }*/
+
 	}
 }
