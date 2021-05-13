47cc6db24ce080a1bb1276e4f7f60c0939e29736
HP <s.k.vrema1655@gmail.com>
2021-05-13 19:25:48 +0530

    Final commit from Main Resource

---
+++ src/test/java/interviewcode/Demo/MasterClass.java
@@ -0,0 +1,10 @@
+package interviewcode.Demo;
+
+public class MasterClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Master branch created");
+	}
+}
